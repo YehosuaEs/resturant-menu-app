fd38b22b2617fc9b68baed1e0f75dd6febfb26a4
YehosuaEs <yehosuaes@gmail.com>
2022-12-19 23:41:30 +0100

    finish the html of add product form

--- src/app/products/new-product/new-product.page.ts
+++ src/app/products/new-product/new-product.page.ts
@@ -1,7 +1,9 @@
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Route, Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Subcategory } from '../product.model';
 
 @Component({
   selector: 'app-new-product',
@@ -10,11 +12,27 @@ import { AlertController } from '@ionic/angular';
 })
 export class NewProductPage implements OnInit {
   form!: FormGroup;
+  subcategory: Subcategory[];
   handlerMessage = '';
   constructor(
     private alertController: AlertController,
+    private loadingController: LoadingController,
+    private productService: ProductService,
     private router: Router
-  ) {}
+  ) {
+    this.subcategory = [
+      { value: 'Botanitas' },
+      { value: 'Burritos' },
+      { value: 'Tacos de plancha' },
+      { value: 'Tacos de guiso' },
+      { value: 'Tacos especiales' },
+      { value: 'Quesadillotas y Tacos XL' },
+      { value: 'Postres caseros' },
+      { value: 'Aguas frescas' },
+      { value: 'Cerveza' },
+      { value: 'Otros' },
+    ];
+  }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -38,9 +56,9 @@ export class NewProductPage implements OnInit {
         updateOn: 'blur',
         validators: [
           Validators.required,
-          Validators.pattern(
-            /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
-          ),
+          // Validators.pattern(
+          //   /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+          // ),
         ],
       }),
       price: new FormControl('', {
@@ -70,6 +88,7 @@ export class NewProductPage implements OnInit {
           handler: () => {
             this.handlerMessage = 'Alert confirmed';
             this.router.navigate(['/products']);
+            this.form.reset();
           },
         },
       ],
