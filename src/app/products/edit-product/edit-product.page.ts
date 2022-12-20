a6b5865d5a1062a2f7de765261e11cfb4000a3a3
YehosuaEs <yehosuaes@gmail.com>
2022-12-20 12:56:42 +0100

    create the form in the edit product  page

--- src/app/products/edit-product/edit-product.page.ts
+++ src/app/products/edit-product/edit-product.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Subcategory } from '../product.model';
 
 @Component({
   selector: 'app-edit-product',
@@ -8,13 +10,60 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./edit-product.page.scss'],
 })
 export class EditProductPage implements OnInit {
+  form!: FormGroup;
+  subcategory: Subcategory[];
   handlerMessage = '';
   constructor(
     private alertController: AlertController,
     private router: Router
-  ) {}
+  ) {
+    this.subcategory = [
+      { value: 'Botanitas' },
+      { value: 'Burritos' },
+      { value: 'Tacos de plancha' },
+      { value: 'Tacos de guiso' },
+      { value: 'Tacos especiales' },
+      { value: 'Quesadillotas y Tacos XL' },
+      { value: 'Postres caseros' },
+      { value: 'Aguas frescas' },
+      { value: 'Cerveza' },
+      { value: 'Otros' },
+    ];
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.minLength(4)],
+      }),
+      category: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required],
+      }),
+      subcategory: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required],
+      }),
+      description: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.minLength(10)],
+      }),
+      img: new FormControl('', {
+        updateOn: 'change',
+        validators: [
+          Validators.required,
+          // Validators.pattern(
+          //   /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+          // ),
+        ],
+      }),
+      price: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.min(0)],
+      }),
+    });
+  }
 
   async onGoBack() {
     const alert = await this.alertController.create({
