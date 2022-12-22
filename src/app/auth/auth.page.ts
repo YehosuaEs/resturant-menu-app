527d7e0135be60f4e200d35426349dbd4cd0423e
YehosuaEs <yehosuaes@gmail.com>
2022-12-22 12:50:09 +0100

    adding styles in the authentication page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,8 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
+import { format } from 'path';
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +11,8 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLogin: boolean = false;
+  isLoading: boolean = false;
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -18,10 +22,41 @@ export class AuthPage implements OnInit {
   ngOnInit() {}
 
   onLogin() {
+    this.isLoading = true;
     this.authService.login();
+    this.loadingCtrl
+      .create({
+        keyboardClose: true,
+        spinner: 'bubbles',
+        message: 'Please wait...',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/restaurant');
+        }, 1000);
+      });
+  }
 
-    setTimeout(() => {
-      this.router.navigateByUrl('/restaurant');
-    }, 1000);
+  onSwitchAuthMode(form: NgForm) {
+    this.isLogin = !this.isLogin;
+    form.resetForm();
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(form.value);
+    if (this.isLogin) {
+      //sent request to login servers
+    } else {
+      //send request to signup servers
+    }
   }
 }
