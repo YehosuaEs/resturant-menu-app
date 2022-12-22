547a4a777a997f9a7cd73cce9515c0c2141752ab
YehosuaEs <yehosuaes@gmail.com>
2022-12-22 11:20:59 +0100

    create a mock login - logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {});
+  }
+  onLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
+  }
 }
